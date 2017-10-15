9eb1ddda5258b2d810c1d465917b275ccccfbc82
petercz1 <email.peter.carroll@gmail.com>
2017-10-15 15:30:41 +0800

    Wedding rings are the world's smallest handcuffs.

--- backend/common/db.php
+++ backend/common/db.php
@@ -17,4 +17,15 @@ if ($conn->connect_error) {
 $sql = "SELECT last_name FROM students";
 $result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+
 echo "Backend connected successfully";
