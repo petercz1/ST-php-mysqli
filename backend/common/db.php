ed202564ede1419e27729e39f87145ec371afbff
petercz1 <email.peter.carroll@gmail.com>
2017-10-15 15:18:43 +0800

    WARNING TO ALL PERSONNEL:
    Firings will continue until morale improves.

--- backend/common/db.php
+++ backend/common/db.php
@@ -2,8 +2,18 @@
 
 // delete before deploy!
 include('logger.php');
-
+include(credentials.php);
 mysqli_report(MYSQLI_REPORT_ALL);
 
+$servername = "localhost";
+$username = $admin;
+$password = $pass;
 
-echo 'check';
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
