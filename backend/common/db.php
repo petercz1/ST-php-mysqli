619bb3c45567caf0ffc8333d39f4750de5b8700d
petercz1 <email.peter.carroll@gmail.com>
2017-10-15 15:23:04 +0800

    "... all the modern inconveniences ..." 		-- Mark Twain

--- backend/common/db.php
+++ backend/common/db.php
@@ -6,11 +6,9 @@ include('credentials.php');
 mysqli_report(MYSQLI_REPORT_ALL);
 
 $servername = "localhost";
-$username = $admin;
-$password = $pass;
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $admin, $pass, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
